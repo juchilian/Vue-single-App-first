a7ac201a2441929e04d6ffe377fd70795a5bc697
juchilian <juchilian@outlook.jp>
2019-10-18 08:26:26 +0900

    最初のストアへの移行完了　後はメソッドとv-modelの最適化

--- memo/src/page/Items.vue
+++ memo/src/page/Items.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <filter-form
-      :form-state="formState"
+      :form-state="sharedState.formState"
       :trips-filtered="tripsFiltered"
       :display-activea-list="displayActiveList"
-      :tag-list="tagList"
+      :tag-list="sharedState.tagList"
       @toggle="toggle"
     ></filter-form>
     <trip-view :trips-filtered="tripsFiltered"></trip-view>
@@ -14,16 +14,68 @@
 <script>
 import FilterForm from "../components/FilterForm";
 import TripView from "../components/TripView";
+import store from '../store'
 export default {
-  props: {
-    tagList: Array,
-    formState: Object,
-    tripsFiltered: Array,
-    displayActiveList: Array
+  data() {
+    return {
+      sharedState: store.state
+    }
+  },
+  computed: {
+    tripsFiltered: function() {
+      // tripList並び替え
+      if (this.sharedState.formState.sort === "cost-low") {
+        this.sharedState.tripList.sort(function(a, b) {
+          if (a.cost < b.cost) return -1;
+          if (a.cost > b.cost) return 1;
+          return 0;
+        });
+      } else if (this.sharedState.formState.sort === "cost-high") {
+        this.sharedState.tripList.sort(function(a, b) {
+          if (a.cost < b.cost) return 1;
+          if (a.cost > b.cost) return -1;
+          return 0;
+        });
+      }
+      let filteredTrips = [];
+      if (this.sharedState.activeList.length === 0) {
+        return this.sharedState.tripList;
+      }
+      this.sharedState.tripList.filter(trip => {
+        // active タグが一致しているか
+        const judgeList = [];
+        this.sharedState.activeList.filter(activeTag => {
+          if (trip.tags.indexOf(activeTag.id) !== -1) {
+            judgeList.push(true);
+            // trip.tags.push(activeTag);
+          } else {
+            judgeList.push(false);
+          }
+        });
+        // every ==> some でor検索
+        if (this.sharedState.formState.searchType === true) {
+          if (judgeList.every(val => val === true)) {
+            filteredTrips.push(trip);
+          }
+        } else {
+          if (judgeList.some(val => val === true)) {
+            filteredTrips.push(trip);
+          }
+        }
+      });
+      return filteredTrips;
+    },
+    displayActiveList: function() {
+      let displayActiveList = [];
+      this.sharedState.activeList.filter(tag => {
+        displayActiveList.push(tag.name);
+      });
+      return displayActiveList;
+    }
   },
   methods: {
     toggle(id) {
-      this.$emit("toggle", id);
+      store.actions.toggle(id)
     }
   },
   components: {
--- memo/src/store.js
+++ memo/src/store.js
@@ -1,16 +1,98 @@
 const state = {
-  trips =[]
+  activeList: [],
+  tagList: [
+    { id: 0, name: "cheep", isActive: false },
+    { id: 1, name: "popular", isActive: false },
+    { id: 2, name: "active", isActive: false },
+    { id: 3, name: "Near Station", isActive: false }
+  ],
+  tripList: [
+    {
+      id: 1,
+      title: "朗らか旅",
+      content: "とても楽しい雰囲気でお勧めです",
+      tags: [0, 1],
+      cost: 1920000
+    },
+    {
+      id: 2,
+      title: "愉快な旅",
+      content: "とても楽しい雰囲気でお勧めです",
+      tags: [2, 3],
+      cost: 150000
+    },
+    {
+      id: 3,
+      title: "ぼんやり旅",
+      content: "とても楽しい雰囲気でお勧めです",
+      tags: [0],
+      cost: 170000
+    },
+    {
+      id: 4,
+      title: "超アクティビティ！旅",
+      content: "とても楽しい雰囲気でお勧めです",
+      tags: [],
+      cost: 600000
+    },
+    {
+      id: 5,
+      title: "100%幸せになれる旅",
+      content: "とても楽しい雰囲気でお勧めです",
+      tags: [3, 4],
+      cost: 90000
+    },
+    {
+      id: 6,
+      title: "海巡り旅",
+      content: "とても楽しい雰囲気でお勧めです",
+      tags: [2],
+      cost: 4500
+    },
+    {
+      id: 7,
+      title: "食べ歩き旅",
+      content: "とても楽しい雰囲気でお勧めです",
+      tags: [1, 2, 3, 4],
+      cost: 198000
+    },
+    {
+      id: 8,
+      title: "ぶらり旅",
+      content: "とても楽しい雰囲気でお勧めです",
+      tags: [2, 4],
+      cost: 100000000
+    },
+    {
+      id: 9,
+      title: "星をみる旅",
+      content: "とても楽しい雰囲気でお勧めです",
+      tags: [],
+      cost: 70000
+    },
+    {
+      id: 10,
+      title: "よき旅",
+      content: "とても楽しい雰囲気でお勧めです",
+      tags: [1, 3],
+      cost: 20000
+    }
+  ],
+  formState: {
+    searchType: false,
+    sort: "cost-low"
+  }
 }
 
 const debug = process.env.NODE_ENV !== 'production'
 const actions = {
   toggle(id) {
     debug && console.log(`toggle triggered with`, id)
-    this.tagList[id].isActive = !this.tagList[id].isActive;
-    this.activeList = [];
-    this.tagList.filter(tag => {
+    state.tagList[id].isActive = !state.tagList[id].isActive;
+    state.activeList = [];
+    state.tagList.filter(tag => {
       if (tag.isActive === true) {
-        this.activeList.push(tag);
+        state.activeList.push(tag);
       }
     });
   }
